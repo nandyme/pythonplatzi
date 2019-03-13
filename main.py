02f98a347ceead2dc122dd87f8841126e37c2dd2
nandyme <fernandomellone2@gmail.com>
2019-03-13 13:38:54 -0300

    reemplazando repo por el de la oficina

--- main.py
+++ main.py
@@ -1,74 +1,123 @@
-clients = 'nandy,nelsi,'
 
 
-def create_client():
+clients = 'sergio,jorge,ruben,'
+
+
+def create_client(client_name):
 	global clients
-	new = _get_new_client()
-	if new not in clients:
-		clients += new
+	
+	if client_name not in clients:
+		clients += client_name
 		_add_comma()
 	else:
-		print('The client is already exist in client list.')
+		print('That client already is in clients list.')
 
 
-def update_client():
+
+
+def update_client(client_name):
 	global clients
 
-	old_client = _get_new_client()
-	if old_client in clients:
-		client = input('Enter the new name of the client: ')
-		clients = clients.replace(old_client + ',', client + ',')
-	else:
-		print('The client is not in the client list yet.')
-		print('Do you want to add it now?')
-		selection = input('Select [Y]es or [N]ot: ')
-		selection = selection.upper()
 
-		if selection == 'Y':
-			clients += old_client
-			_add_comma()
+	if client_name in clients:
+		updated_client_name = input('What is the new name for the client? ')
+		clients = clients.replace(client_name + ',', updated_client_name + ',')
+	else:
+		_client_is_not_in_list(client_name)
+
+
+def delete_client(client_name):
+	global clients
+
+	if client_name in clients:
+		clients = clients.replace(client_name + ',','')
+	else:
+		_client_is_not_in_list()
+
+
+def searc_client(client_name):
+	global clients
+
+	if client_name in clients:
+		print('The client is in clients list.')
+		print('What would you like to do? ')
+		_ask_for_modify_clients_list()
+		print('[N]othing')
+		command = input()
+		command = command.upper()
+		if command == 'U' :
+			update_client(client_name)
+		elif command == 'D':
+			delete_client(client_name)
 		else:
 			pass
-
-def del_client():
-	global clients
-	to_del = _get_new_client()
-	if to_del in clients:
-		clients = clients.replace(to_del + ',', '')
 	else:
-		print('The client is not in clients list.')
-
-def welcome():
-	print('WELCOME TO PLATZI VENTAS')
-	print('*' * 50)
-	print('What would yo want to do? ')
-	print('[C]reate client')
-	print('[U]pdate client')
-	print('[D]elete')
+		_client_is_not_in_list(client_name)
 
 
-def _get_new_client():
-	return input('What is the name of the client? ')
-
-
-
-def _add_comma(): 
+def _add_comma():
 	global clients
+
 	clients += ','
 
 
-if __name__ == '__main__':
-	welcome()
-	command = input('Ingress your selection: ')
-	command = command.upper()
-	
-	if command == 'C':
-		create_client()
-	elif command == 'D':
-		del_client()
-	elif command == 'U':
-		update_client()
+def _client_is_not_in_list(client_name):
+	print('Client is not in clients list.')
+	print('Do you want to add it now? ')
+	selection = _yes_or_not()
+	if selection == 'Y':
+		create_client(client_name)
 	else:
-		print('Incorrect command.')
+		pass
+
+
+def _yes_or_not():
+	#returns a selection command Y or N in a string
+	selection = input('[Y]es or [N]ot: ')
+	selection = selection.upper()
+	return selection
+
+def _get_client_name():
+	return input('What is the client name? ')
+
+
+def list_clients():
+	global clients
 
 	print(clients)
+
+def _print_welcome():
+	print('WELCOME TO PLATZI VENTAS')
+	print('*' * 50)
+	print('What would you like to do? ')
+	print('[C]reate client')
+	_ask_for_modify_clients_list()
+	print('[S]earch client')
+
+
+def _ask_for_modify_clients_list():
+	print('[U]pdate client')
+	print('[D]elete client')
+
+
+if __name__ == '__main__':
+	_print_welcome()
+
+	command = input()
+	command = command.upper()
+
+	if command == 'C':
+		client_name = _get_client_name()
+		create_client(client_name)
+		list_clients()
+	elif command == 'D':
+		delete_client(_get_client_name())
+	elif command == 'U':
+		update_client(_get_client_name())
+	elif command == 'S':
+		searc_client(_get_client_name())
+	else:
+		print('Invalid command.')
+
+	
+	list_clients()
